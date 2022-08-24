0afb4e6a133e256cc8d0fdd54546b91a8e61ee98
Rivu Dass <techiehomework@gmail.com>
2022-08-24 13:47:58 +0530

    aasd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Checking gt");
+            Console.WriteLine("Checking gggt");
             Console.ReadLine();
         }
     }

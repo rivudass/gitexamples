69e2de9fbdedf8921e9c1f4c00cb31529c6bfa65
rivudass <111951202+rivudass@users.noreply.github.com>
2022-08-24 13:49:23 +0530

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Checking ggt");
+            Console.WriteLine("Checking giit");
             Console.ReadLine();
         }
     }
